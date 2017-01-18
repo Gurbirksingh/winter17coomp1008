83360cd0f78b17dc9afdf2e06500542e9235a77f
apple <apple@10.250.20.34>
2017-01-18 14:52:20 -0500

    updated

--- src/winter17jan18/Winter17jan18.java
+++ src/winter17jan18/Winter17jan18.java
@@ -5,6 +5,8 @@
  */
 package winter17jan18;
 
+import java.util.Scanner;
+
 /**
  *
  * @author apple
@@ -16,8 +18,33 @@ public class Winter17jan18 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       //enter first fav app
+        System.out.printf("What is ur  1 favourite app?");
+        Scanner keyboard = new Scanner(System.in);
+        String firstapp = keyboard.nextLine();
+         
+        //enter 2 fav app
+        System.out.printf("What is ur 2 favourite app?");
+        String secondapp = keyboard.nextLine();
         
-        System.out.printf("hello world welcome");
+        //enter 3 fav app
+         System.out.printf("What is ur  3 favourite app?");
+        String thirdapp = keyboard.nextLine();
+        
+        //enter 4 fav app
+         System.out.printf("What is ur 4 favourite app?");
+        String fourthapp = keyboard.nextLine();
+        
+        //enter 5 fav app
+         System.out.printf("What is ur 5 favourite app?");
+        String fifthapp = keyboard.nextLine();
+        
+        //print all app names
+        System.out.printf("Your favourite apps are:"
+                              + firstapp + " ," + secondapp + " ," + thirdapp + 
+                                 " ,"+ fourthapp + " ," + fifthapp);
+        
+        
     }
     
 }
